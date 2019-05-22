628b6503dd5d24bc4f574b13e9e53aac8fcafa0a
Pablo <45538982+Pabloitl@users.noreply.github.com>
2019-05-21 21:43:20 -0500

    Added serialization problem

---
+++ Retos/src/serialization/Serialization_binary_tree.py
@@ -0,0 +1,31 @@
+# Class node
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+# Algorithm
+def serialize(root, serie):
+    if root == None:
+        serie.append(root)
+        return
+    serie.append(root.val)
+    serialize(root.left, serie)
+    serialize(root.right, serie)
+    return serie
+
+def deserialize(serie):
+        def _helper(index):
+            if serie[index] == None:
+                return None, index + 1
+
+            value = serie[index]
+            left, index = _helper(index + 1)
+            right, index = _helper(index)
+            return Node(value, left, right), index
+        return _helper(0)[0]
+
+# Test
+node = Node('root', Node('left', Node('left.left')), Node('right'))
+assert deserialize(serialize(node, list())).left.left.val == 'left.left'
